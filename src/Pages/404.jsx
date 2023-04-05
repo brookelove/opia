ba91e3401130410a1a53d6507e4cac435d3b93ae
brookelove <sblove@bu.edu>
2023-04-04 21:43:03 -0700

    adding capture of image to the page

---
+++ src/Pages/404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Lost() {
+return (
+    <div>
+        <h1>404 Not Found</h1>
+        <a href="/">Home</a>
+    </div>
+)
+}
+
+export default Lost
--- src/Pages/Face.jsx
+++ src/Pages/Face.jsx
@@ -1,24 +1,54 @@
-import React, {useEffect, useRef}from "react";
+import React, { useEffect, useRef } from "react";
+import Webcam from "react-webcam";
 
 const Face = () => {
-    const cameraRef = useRef(null);
+  const cameraRef = useRef(null);
+  const photoRef = useRef(null);
+  const stripRef = useRef(null);
 
-    const getPhoto = ()=> {
-        navigator.mediaDevices.getUserMedia({video: {width:300}}).then(stream =>{
-            let camera = cameraRef.current;
-            camera.srcObject = stream;
-            camera.play();
-        })
-    }
-    useEffect(()=> {
-        getPhoto();
-    },[cameraRef])
- return (
+  const getPhoto = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 700 } })
+      .then((stream) => {
+        let camera = cameraRef.current;
+        camera.srcObject = stream;
+        camera.play();
+      })
+      .catch((err) => {
+        console.error("error:", err);
+      });
+  };
+
+  const takePhoto = () => {
+    let photo = photoRef.current;
+    let strip = stripRef.current;
+
+    const data = photoRef.current.getScreenshot();
+    console.warn(data);
+    const link = document.createElement("a");
+    link.href = data;
+    link.setAttribute("download", "myWebcam");
+    link.innerHTML = `<img src='${data}' alt='thumbnail'/>`;
+    strip.insertBefore(link, strip.firstChild);
+  };
+
+  useEffect(() => {
+    getPhoto();
+  }, []);
+
+  return (
     <div>
-        <h1>Face</h1>
-        <video ref={cameraRef}></video>
-        <button>OPIA</button>
+      <h1>Face</h1>
+    <Webcam screenshotFormat="image/jpeg" ref={photoRef} />
+      <button onClick={takePhoto}>OPIA</button>
+      <div ref={stripRef}></div>
     </div>
- )
-}
-export default Face
+  );
+};
+
+export default Face;
+
+
+
+
+
